0ec8213714bb6aef9fe2f045b70626da1b19d247
Mathias Aivasovsky <mathiasaivasovsky@gmail.com>
2022-09-21 09:36:39 -0400

    Initial API build

---
+++ src/API/API.js
@@ -0,0 +1,90 @@
+const API_URL = "https://www.reddit.com";
+
+const intialOptions = {
+  headers: {
+    accept: "application/json",
+  },
+};
+
+export const getSubreddit = async (subreddit, options = intialOptions) => {
+  const url = `${API_URL}/r${subreddit}/about.json`;
+
+  const query = await fetch(url, options);
+  const jsonResponse = await query.json();
+  if (query.ok) return jsonResponse.data;
+
+  throw new Error("Failed to fetch subreddit: " + subreddit);
+};
+
+export const getSubredditPosts = async (subreddit, options = intialOptions) => {
+  const url = `${API_URL}/r${subreddit}.json`;
+
+  const query = await fetch(url, options);
+  const jsonResponse = await query.json();
+  if (query.ok)
+    return {
+      after: jsonResponse.data.after,
+      before: jsonResponse.data.before,
+      posts: jsonResponse.data.children,
+    };
+
+  throw new Error("Failed to fetch posts from: " + subreddit);
+};
+
+export const getAllSubreddits = async (options = intialOptions) => {
+  const url = `${API_URL}/subreddits.json`;
+
+  const query = await fetch(url, options);
+  const jsonResponse = await query.json();
+  if (query.ok)
+    return {
+      after: jsonResponse.data.after,
+      before: jsonResponse.data.before,
+      subreddits: jsonResponse.data.map((sbr) => sbr.data),
+    };
+
+  throw new Error("Failed to fetch subreddits");
+};
+
+export const getPost = async (post, options = intialOptions) => {
+  const [subreddit, id] = post;
+  const url = `${API_URL}/r/${subreddit}/comments/${id}/.json`;
+
+  const query = await fetch(url, options);
+  const jsonResponse = await query.json();
+  if (query.ok)
+    return {
+      after: jsonResponse[0].data.after,
+      before: jsonResponse[0].data.before,
+      subreddits: jsonResponse[0].data.map((pst) => pst.data),
+    };
+
+  throw new Error("Failed to fetch post");
+};
+
+export const getComments = async (post, options = intialOptions) => {
+  const [subreddit, id] = post;
+  const url = `${API_URL}/r/${subreddit}/comments/${id}/.json`;
+
+  const query = await fetch(url, options);
+  const jsonResponse = await query.json();
+  if (query.ok)
+    return {
+      after: jsonResponse[1].data.after,
+      before: jsonResponse[1].data.before,
+      subreddits: jsonResponse[1].data.map((cmt) => cmt.data),
+    };
+
+  throw new Error("Failed to fetch comments");
+};
+
+export const getUser = async (username, options = intialOptions) => {
+  const url = `${API_URL}/user/${username}/about.json`;
+
+  const query = await fetch(url, options);
+  const jsonResponse = await query.json();
+  if (query.ok) return jsonResponse.data;
+    
+
+  throw new Error("Failed to fetch user");
+};
