ffb182cbf0726886ae59bf3678dce3b1077c0f56
Mathias Aivasovsky <mathiasaivasovsky@gmail.com>
2022-09-21 09:41:07 -0400

    Added Routing / Changed Styling

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { NavBar } from "./components/navBar/navbar.js";
-import { Posts } from "./components/posts/posts.js";
+import { Post } from "./features/posts/posts.js";
 import { Side } from "./components/side/side.js";
+import { CurrentPost } from "./features/currentPost/currentPost.js";
+import store from "./store";
 import "./AppStyle.css";
+import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 function App() {
   return (
-    <Router>
-      <nav>
+    <Provider store={store}>
+      <Router>
         <NavBar id="NavBar" />
-      </nav>
-      <div className="article">
-          <Posts />
-          <Side />
-      </div>
-    </Router>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <div className="article">
+                <div className="Posts">
+                  <Post />
+                </div>
+                <Side />
+              </div>
+            }
+          />
+          <Route path={"/post/:id"} element={<CurrentPost />} />
+        </Routes>
+      </Router>
+    </Provider>
   );
 }
 
